9c0258fb488c0b4320d751494c51e3802433b3d0
Jessica Garrido <jessica.garrido.di@gmail.com>
2025-02-13 18:38:40 +0100

    A few tests on the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 function Home() {
   return (
-    <div>
-      <h1>Page d'Accueil</h1>
-      <p>Bienvenue sur la page principale de mon site !</p>
+    <div className={`${styles["home-page"]} df aic jcc`}>
+      <section className={`${styles["home-banner"]} dfull`}>
+        <div>
+          <h1>Et la boucle est bouclée !</h1>
+          <h3>Bonjour cher internaute !</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <p>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </div>
+        <div className={styles["image-container"]}>
+          <img src={require("../assets/home-bg.png")} alt="Logo" />
+        </div>
+      </section>
     </div>
   );
 }
 
 export default Home;
-
